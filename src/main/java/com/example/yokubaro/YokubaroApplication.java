2f84cf585667a9dd82d41eb934222691e15fb07f
Natsuki Yamazaki <lanternyama+git@gmail.com>
2026-09-11 14:35:54 +0900

    first commit

---
+++ src/main/java/com/example/yokubaro/YokubaroApplication.java
@@ -0,0 +1,13 @@
+package com.example.yokubaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YokubaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YokubaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yokubaro/YokubaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yokubaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YokubaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
