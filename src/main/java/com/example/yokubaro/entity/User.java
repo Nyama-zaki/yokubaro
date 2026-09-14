75f688ef6f1dc70495ede09015acaa45cf898161
Natsuki Yamazaki <lanternyama+git@gmail.com>
2026-09-14 17:57:40 +0900

    UserRepositoryとPostRepositoryを追加とそれに伴う設定の調整

---
+++ src/main/java/com/example/yokubaro/entity/User.java
@@ -0,0 +1,26 @@
+package com.example.yokubaro.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "login_id")
+    private String loginId;
+
+    private String password;
+
+    private String userName;
+}
---
+++ src/main/java/com/example/yokubaro/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.yokubaro.repository;
+
+import com.example.yokubaro.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
+//この継承でSpring Bootが用意してくれているJpaRepositoryを継承することによって
+// 「保存する(save)」「全件検索する(findAll)」「IDで探す(findById)」といった基本のデータベース操作機能が、まるごと全部自動で使える
+
